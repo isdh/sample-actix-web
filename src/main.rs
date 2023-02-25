e7340d8df9a3f289234e6372f42decd9ef8bbf83
isdh <hideorseekyes+github@gmail.com>
2023-02-26 00:55:20 +0900

    impl: sample web and hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ get, HttpResponse, HttpServer, Responder, App};
+
+#[get("/")]
+// ここの関数名とサービスの関数名は合わせる必要がある
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().service(index))
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
